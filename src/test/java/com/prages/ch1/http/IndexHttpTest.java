1efe2c22e67e046970fc064495d471bfae8c5814
kwangsik <lks21c@gmail.com>
2016-02-02 11:51:29 +0900

    change

--- src/test/java/com/prages/ch1/http/IndexHttpTest.java
+++ src/test/java/com/prages/ch1/http/IndexHttpTest.java
@@ -20,15 +20,15 @@ public class IndexHttpTest extends AbstractBaseHttpTest {
         StringBuilder sb = new StringBuilder();
         sb.append("{");
         sb.append("\"settings\":" + resourceFileReadUtil.getFileContent("prages/ch1/schema/price_detail_settings.json"));
-        sb.append(", " + "\"mappings:\"" + resourceFileReadUtil.getFileContent("prages/ch1/schema/price_detail_mappings.json"));
+        sb.append(", " + "\"mappings\":" + resourceFileReadUtil.getFileContent("prages/ch1/schema/price_detail_mappings.json"));
         sb.append("}");
-        HttpClientResponse response = simpleHttpUtil.request("POST", sb.toString());
+        HttpClientResponse response = simpleHttpUtil.request("POST", "/priceinfo/info", sb.toString());
         System.out.println(response.response());
     }
 
     @Test
     public void testIndex() throws Exception {
-        HttpClientResponse response = simpleHttpUtil.request("PUT", resourceFileReadUtil.getFileContent("prages/ch1/schema/price_index.json"));
+        HttpClientResponse response = simpleHttpUtil.request("POST", "/priceinfo/info", resourceFileReadUtil.getFileContent("prages/ch1/schema/price_index.json"));
         System.out.println(response.response());
     }
 }
